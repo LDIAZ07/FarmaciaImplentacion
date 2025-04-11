01dc0a6ae1543455577f40a47fb5c92ceaf37b6e
LDIAZ07 <louisdays845@gmail.com>
2025-04-10 18:50:11 -0600

    Hotfix: ruta de web

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-    // return view('login.login');
+    // return view('welcome');
+    return view('login.login');
 });
 
 Route::get('/farmacia', function () {
